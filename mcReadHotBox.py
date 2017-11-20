28b46fa0a2bc0c0019fee93b53eada0692667d34
Mei Chu <meimchu@gmail.com>
2017-11-19 21:09:50 -0800

    Added colour to RGB Changed each label's size Moved non RGBA channels to another column

--- mcReadHotBox.py
+++ mcReadHotBox.py
@@ -1,5 +1,5 @@
 # Created by: Mei Chu
-# Last updated: November 16, 2017
+# Last updated: November 19, 2017
 
 try:
     from PySide.QtGui import *
@@ -22,10 +22,10 @@ class Panel(QDialog):
         self.layout = QGridLayout()
         mouse_position = QCursor.pos()
         self.move(mouse_position - QPoint(100, 100))
-        self.resize(400, 400)
+        self.resize(150, 40)
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
-        self.layout.setVerticalSpacing(20)
+        self.layout.setVerticalSpacing(5)
         self.setAttribute(Qt.WA_QuitOnClose)
 
         global hotbox_count
@@ -33,8 +33,23 @@ class Panel(QDialog):
         mcRead = nuke.selectedNode()
         mcReadLength = len(mcRead.channels())
         while hotbox_count < mcReadLength:
-            self.layout.addWidget(ActionLabel(), hotbox_count, 0)
+            mcReadData = mcRead.channels()
+            if mcReadData[hotbox_count] == "rgba.red":
+                self.layout.addWidget(ActionLabel(), hotbox_count, 0)
+
+            elif mcReadData[hotbox_count] == "rgba.green":
+                self.layout.addWidget(ActionLabel(), hotbox_count, 0)
+
+            elif mcReadData[hotbox_count] == "rgba.blue":
+                self.layout.addWidget(ActionLabel(), hotbox_count, 0)
+
+            elif mcReadData[hotbox_count] == "rgba.alpha":
+                self.layout.addWidget(ActionLabel(), hotbox_count, 0)
+
+            else:
+                self.layout.addWidget(ActionLabel(), hotbox_count - 4, 1)
             hotbox_count += 1
+
         self.layout.addWidget(QLabel())
         self.setLayout(self.layout)
 
@@ -44,7 +59,7 @@ class ActionLabel(QLabel):
         self.setAlignment(Qt.AlignCenter)
         self.setMouseTracking(True)
         self.setFixedWidth(100)
-        self.setFixedHeight(25)
+        self.setFixedHeight(40)
         self.setStyleSheet("background:grey;color:white")
         global hotbox_count
 
@@ -52,6 +67,14 @@ class ActionLabel(QLabel):
         mcReadData = mcRead.channels()
 
         self.setText(mcReadData[hotbox_count])
+        if mcReadData[hotbox_count] == "rgba.red":
+            self.setStyleSheet("background:red;color:white")
+
+        elif mcReadData[hotbox_count] == "rgba.green":
+            self.setStyleSheet("background:green;color:white")
+
+        elif mcReadData[hotbox_count] == "rgba.blue":
+            self.setStyleSheet("background:blue;color:white")
 
 def main():
     panel = Panel()
