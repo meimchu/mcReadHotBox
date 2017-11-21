96c50012d10537818e87552727ba89ac18dd508b
Mei Chu <meimchu@gmail.com>
2017-11-20 22:40:53 -0800

    Dynamic colouring and column/row counts

--- mcReadHotBox.py
+++ mcReadHotBox.py
@@ -15,6 +15,8 @@ except:
 import nuke
 
 hotbox_count = 0
+col_count = 1
+row_count = 0
 
 class Panel(QDialog):
     def __init__(self):
@@ -22,13 +24,16 @@ class Panel(QDialog):
         self.layout = QGridLayout()
         mouse_position = QCursor.pos()
         self.move(mouse_position - QPoint(100, 100))
-        self.resize(150, 40)
+        self.resize(150, 30)
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.layout.setVerticalSpacing(5)
         self.setAttribute(Qt.WA_QuitOnClose)
+        self.setMouseTracking(True)
 
         global hotbox_count
+        global col_count
+        global row_count
 
         mcRead = nuke.selectedNode()
         mcReadLength = len(mcRead.channels())
@@ -47,7 +52,11 @@ class Panel(QDialog):
                 self.layout.addWidget(ActionLabel(), hotbox_count, 0)
 
             else:
-                self.layout.addWidget(ActionLabel(), hotbox_count - 4, 1)
+                self.layout.addWidget(ActionLabel(), row_count, col_count)
+                row_count += 1
+                if (hotbox_count + 1) % 4 == 0:
+                    col_count += 1
+                    row_count = 0
             hotbox_count += 1
 
         self.layout.addWidget(QLabel())
@@ -58,15 +67,16 @@ class ActionLabel(QLabel):
         super(ActionLabel, self).__init__()
         self.setAlignment(Qt.AlignCenter)
         self.setMouseTracking(True)
-        self.setFixedWidth(100)
-        self.setFixedHeight(40)
+        self.setFixedWidth(150)
+        self.setFixedHeight(30)
         self.setStyleSheet("background:grey;color:white")
         global hotbox_count
 
         mcRead = nuke.selectedNode()
         mcReadData = mcRead.channels()
-
-        self.setText(mcReadData[hotbox_count])
+        hotbox_name = mcReadData[hotbox_count]
+        self.setText(hotbox_name)
+        self.setObjectName(hotbox_name)
         if mcReadData[hotbox_count] == "rgba.red":
             self.setStyleSheet("background:red;color:white")
 
@@ -74,6 +84,22 @@ class ActionLabel(QLabel):
             self.setStyleSheet("background:green;color:white")
 
         elif mcReadData[hotbox_count] == "rgba.blue":
+            self.setStyleSheet("background:blue;color:white")
+
+    def enterEvent(self, event):
+
+        self.setStyleSheet('background:orange;color:white')
+
+    def leaveEvent(self, event):
+
+        self.setStyleSheet('background:grey;color:white')
+        if self.text() == "rgba.red":
+            self.setStyleSheet("background:red;color:white")
+
+        elif self.text() == "rgba.green":
+            self.setStyleSheet("background:green;color:white")
+
+        elif self.text() == "rgba.blue":
             self.setStyleSheet("background:blue;color:white")
 
 def main():
