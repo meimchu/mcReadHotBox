23a42c240c99179dd21901f38971407af41207b0
Mei Chu <meimchu@gmail.com>
2018-09-21 00:35:04 -0700

    Made an efficiency update and also removed individual subchannel selection. Only main channel selection available now.

--- mcReadHotBox.py
+++ mcReadHotBox.py
@@ -1,8 +1,10 @@
 # Created by: Mei Chu
-# Last updated: September 19, 2018
-# Version: 0.1.2
+# Last updated: September 20, 2018
+# Version: 0.1.3
 
 import nuke
+import subprocess
+import re
 
 try:
     from PySide.QtGui import *
@@ -12,6 +14,10 @@ except Exception:
     from PySide2.QtWidgets import *
     from PySide2.QtGui import *
     from PySide2.QtCore import *
+
+# Global variable for the size of the labels and search box inside the panel
+fixed_width = 150
+fixed_height = 30
 
 
 def set_default_colour(hotbox, search_box):
@@ -26,32 +32,27 @@ class Panel(QDialog):
     def __init__(self, hotbox_count, col_count, row_count):
         super(Panel, self).__init__()
 
-        # Setting the look of panel
-        self.setObjectName('widget_panel')
-        self.layout = QGridLayout()
-        self.layout.setVerticalSpacing(5)
-        self.resize(150, 30)
-        self.setWindowFlags(Qt.FramelessWindowHint)
-        self.setAttribute(Qt.WA_QuitOnClose)
-        # self.setAttribute(Qt.WA_TranslucentBackground)
-
-        # Move panel to beside current mouse position
-        self.mouse_position = QCursor.pos()
-        self.move(self.mouse_position)
-
-        # Adding search box at the start
-        self.search_box = SearchBox(self)
-        self.layout.addWidget(self.search_box, 0, 0)
-
         # Bring in global variables as needed
         self.hotbox_count = hotbox_count
         self.col_count = col_count
         self.row_count = row_count
 
-        # Setting variable for the selected node
+        # Set the look of panel
+        self.setObjectName('widget_panel')
+        self.layout = QGridLayout()
+        self.layout.setVerticalSpacing(5)
+        self.setWindowFlags(Qt.Popup)
+        # self.setWindowFlags(Qt.FramelessWindowHint)
+        # self.setAttribute(Qt.WA_TranslucentBackground)
+
+        # Add search box at the start
+        self.search_box = SearchBox(self)
+        self.layout.addWidget(self.search_box, 0, 0)
+
+        # Set variable for the selected node
         self.mc_read = nuke.selectedNode()
 
-        # Determining the amount of channels available at selected node
+        # Determine the amount of channels available at selected node
         self.read_data = []
         for channel in self.mc_read.channels():
             main_channel = channel.split('.')[0]
@@ -66,8 +67,9 @@ class Panel(QDialog):
 
             rgba_list = ['rgba.red', 'rgba.green', 'rgba.blue', 'rgba.alpha']
 
+            self.label = ActionLabel(self.hotbox_name, self, self.search_box)
             if self.hotbox_name in rgba_list:
-                self.layout.addWidget(ActionLabel(self.hotbox_name, self, self.search_box), self.row_count, 0)
+                self.layout.addWidget(self.label, self.row_count, 0)
 
             else:
                 # Every time 10 hot boxes appear, move to the next column
@@ -75,47 +77,99 @@ class Panel(QDialog):
                     self.col_count += 1
                     self.row_count = 0
 
-                self.layout.addWidget(ActionLabel(self.hotbox_name, self, self.search_box), self.row_count, self.col_count)
+                self.layout.addWidget(self.label, self.row_count, self.col_count)
 
             self.hotbox_count += 1
             self.row_count += 1
 
         self.setLayout(self.layout)
 
+        # Functions that will move the panel around based on cursor position
+        self.monitor_res = self.get_resolution()
+        self.cursor_position = self.get_mouse_position()
+        self.move_panel()
+
+        # Set search box as the main focus
+        self.search_box.setFocus()
+
+    def get_mouse_position(self):
+        # Move panel to beside current mouse position
+        x_pos = QCursor.pos().x()
+        y_pos = QCursor.pos().y()
+        return tuple([x_pos, y_pos])
+        # self.move(self.cursor_position - QPoint(5, 5))
+
+    def get_resolution(self):
+        results = str(subprocess.Popen(['system_profiler SPDisplaysDataType'], stdout=subprocess.PIPE, shell=True).communicate()[0])
+        res = re.search('Resolution: \d* x \d*', results).group(0).split(' ')
+        width, height = int(res[1]) / 2, int(res[3]) / 2
+
+        return tuple([width, height])
+
+    def move_panel(self):
+        x_cursor = self.cursor_position[0]
+        y_cursor = self.cursor_position[1]
+        x_monitor = self.monitor_res[0]
+        y_monitor = self.monitor_res[1]
+
+        if self.col_count == 0:
+            y_panel_size = int(self.row_count * fixed_height)
+
+        else:
+            y_panel_size = int(10 * fixed_height)
+
+        x_panel_size = (self.col_count + 1) * fixed_width
+
+        # Move panel if cursor's Y position plus panel size goes off the edge
+        if (y_cursor + y_panel_size) > y_monitor:
+            y_pos = y_cursor - y_panel_size
+
+        else:
+            y_pos = y_cursor
+
+        # Move panel if cursor's X position plus panel size goes off the edge
+        if (x_cursor + x_panel_size) > x_monitor:
+            x_pos = x_cursor - x_panel_size
+
+        else:
+            x_pos = x_cursor
+
+        self.move(QPoint(x_pos, y_pos))
+
 
 class ActionLabel(QLabel):
     def __init__(self, hotbox_name, panel, search_box):
         super(ActionLabel, self).__init__()
-
-        # Setting the look of label
-        self.setAlignment(Qt.AlignCenter)
-        self.setMouseTracking(True)
-        self.setFixedWidth(150)
-        self.setFixedHeight(30)
 
         # Bring in global variable as needed
         self.hotbox_name = hotbox_name
         self.panel = panel
         self.search_box = search_box
 
-        # Setting variables for the selected node
+        # Set the look of label
+        self.setAlignment(Qt.AlignCenter)
+        self.setMouseTracking(True)
+        self.setFixedWidth(fixed_width)
+        self.setFixedHeight(fixed_height)
+
+        # Set variables for the selected node
         self.mc_read = nuke.selectedNode()
 
-        # Setting text to the label
+        # Set text to the label
         self.setText(self.hotbox_name)
         self.setObjectName(self.hotbox_name)
         self.default_colour()
 
     def default_colour(self):
-        # Setting default label scheme for rgb
+        # Set default label scheme for rgb
         set_default_colour(self, self.search_box)
 
     def enterEvent(self, event):
-        # Setting colour of label when selected
+        # Set colour of label when selected
         self.setStyleSheet('background:orange;color:white')
 
     def leaveEvent(self, event):
-        # Setting colour of label back to default scheme
+        # Set colour of label back to default scheme
         self.default_colour()
 
     def mousePressEvent(self, event):
@@ -144,14 +198,18 @@ class SearchBox(QLineEdit):
     def __init__(self, panel):
         super(SearchBox, self).__init__()
 
+        # Bring in global variables as needed
         self.panel = panel
 
-        # Setting the look of the line edit
+        # Set the look of the line edit
         self.setObjectName('search_box')
         self.setAlignment(Qt.AlignCenter)
         self.setMouseTracking(True)
-        self.setFixedWidth(150)
-        self.setFixedHeight(30)
+        self.setFixedWidth(fixed_width)
+        self.setFixedHeight(fixed_height)
+        self.setText('')
+
+        # Set text change signal
         self.textChanged.connect(self.text_changed)
 
         try:
